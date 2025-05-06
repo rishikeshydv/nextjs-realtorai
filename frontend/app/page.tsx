de6408036671f24e5a017a454a3806d53a620499
rishikeshydv <rishikeshadh4@gmail.com>
2025-05-06 14:23:32 -0400

    feat: backend routes updated

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,25 +4,6 @@ import Section1 from '@/components/homepage/Section1';
 
 export default function Home() {
   
-
-  // async function fetchUser() {
-  // try {
-  //   const response = await fetch("/api/checkcookie");
-  //   console.log("Response: ",response);
-  //   if (!response.ok) throw new Error("Not authenticated");
-
-  //   const data = await response.json();
-  //   setUserEmail(data.username);
-  // } catch {
-  //   // Redirect to login if not authenticated
-  //   // window.location.href = "/auth/login";
-  //   console.log("Not authenticated");
-  // }
-  // }
-  // useEffect(() => {
-  //   fetchUser();
-  // }, []);
-
   return (
     <div>
     <h1>
